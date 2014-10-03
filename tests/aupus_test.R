78ee8f6ef2b5d8c5095aa6b4a500d5a9aa90d835
mkao006 <mkao006@gmail.com>
2014-10-03 13:50:11 +0200

    Added required data described from the documentation

--- tests/aupus_test.R
+++ tests/aupus_test.R
@@ -4,8 +4,8 @@
 library(reshape2)
 library(RJDBC)
 library(data.table)
-## library(FAOSTAT)
-
+library(FAOSTAT)
+options(java.parameters = "-Xmx3000m")
 
 ## Connect to the database
 drv = JDBC(driverClass = "oracle.jdbc.driver.OracleDriver",
@@ -30,22 +30,30 @@ ratio =
 
 
 
+dbGetQuery(conn, "SELECT COUNT(*) FROM item")
+
+swsItemTable = dbGetQuery(conn, "SELECT * FROM item")
+save(swsItemTable, file = "swsItemTable.RData")
+
 
 
 ## Test of Wheat Germany
 testCountryCode = 79
-testItemCode = 15
+## testItemCode = swsItemTable[swsItemTable$GRP_IND == "D", "ITEM"]
+testItemCode = 1:1000
 aupusElements = c(11, 21, 31, 41, 51, 58, 61, 62, 66, 71, 91, 92, 95,
     96, 101, 111, 121, 131, 141, 144, 151, 161, 171, 174, 261, 274, 281,
     284, 541, 546)
 
 getAupusData = function(countryCode, itemCode, elementCode, conn){
-    tmp = dbGetQuery(conn, paste0("SELECT * FROM tsv_ics_work_yr
+    query = paste0("SELECT * FROM tsv_ics_work_yr
                                   WHERE area = '", countryCode, "'
-                                  AND item = '", itemCode, "'
+                                  AND item in (",
+                                  paste0(itemCode, collapse = ", "), ")
                                   AND ele in (",
-        paste0(elementCode, collapse = ", "), ")")
-                     )
+                                  paste0(elementCode, collapse = ", "), ")")
+    print(query)
+    tmp = dbGetQuery(conn, query)
     colnames(tmp)[1:3] =
         c("areaCode", "itemCode", "elementCode")
     melted = melt(tmp,
@@ -63,14 +71,17 @@ getAupusData = function(countryCode, itemCode, elementCode, conn){
     casted
 }
 
-rawAupus = getAupusData(testCountryCode, testItemCode, aupusElements,
-                        conn)
+rawAupus =
+    getAupusData(testCountryCode, testItemCode, aupusElements,
+                 conn)
+save(rawAupus, file = "aupusData.RData")
 
 ## This is not required for primary commodities
 getInputData = function(countryCode, itemCode, conn){
     tmp = dbGetQuery(conn, paste0("SELECT * FROM input_from_procv
                                   WHERE area = '", countryCode, "'
-                                  AND item_child = '", itemCode, "'"))
+                                  AND item_child in (",
+                                  paste0(itemCode, collapse = ", "), ")"))
     colnames(tmp)[1:3] =
         c("areaCode", "itemCode", "elementCode")
     melted = melt(tmp,
@@ -84,14 +95,17 @@ getInputData = function(countryCode, itemCode, conn){
         value.var = "value"))
     valueCol = grep("NUM", colnames(casted), value = TRUE)
     casted[, (valueCol) :=
-               lapply(valueCol, function(x) as.numeric(casted[[x]]))]    
+               lapply(valueCol, function(x) as.numeric(casted[[x]]))]
+    setnames(casted, old = grep("NUM", colnames(input), value = TRUE),
+             new = gsub("NUM", "INPUT", 
+                 grep("NUM", colnames(input), value = TRUE)))
     casted
 }
 input = getInputData(testCountryCode, testItemCode, conn)
+save(input, file = "input.RData")
 
 
-
-getShares = function(countryCode, itemCode, conn){
+getRatios = function(countryCode, itemCode, conn){
     ## This is a temporary solution
     years = 1961:2015
     
@@ -99,7 +113,8 @@ getShares = function(countryCode, itemCode, conn){
     base = dbGetQuery(conn, paste0("SELECT area, item, ele, yr, ratio
                                    FROM aupus_ratios
                                    WHERE area in (", countryCode, ")
-                                   AND item = '", itemCode, "'
+                                   AND item in (",
+                                   paste0(itemCode, collapse = ", "), ")
                                    AND yr != 0"))
     colnames(base) =
         c("areaCode", "itemCode", "elementCode", "Year", "ratio")
@@ -109,7 +124,7 @@ getShares = function(countryCode, itemCode, conn){
         paste0("SELECT area, item, ele, ratio FROM aupus_ratios
                 WHERE area in (", countryCode, ")
                 AND yr = 0
-                AND item = '", itemCode, "'"))
+                AND item in (", paste0(itemCode, collapse = ", "), ")"))
     colnames(wildCardYear) =
         c("areaCode", "itemCode", "elementCode", "wildCardYearRatio")
     expandedWildCardYear =
@@ -125,7 +140,8 @@ getShares = function(countryCode, itemCode, conn){
     ## global wild card
     wildCardGlobal = dbGetQuery(conn,
         paste0("SELECT item, ele, ratio FROM aupus_ratios
-                WHERE area = 0 AND item = '", itemCode, "'"))
+                WHERE area = 0 AND item in (",
+               paste0(itemCode, collapse = ", "), ")"))
     colnames(wildCardGlobal) =
         c("itemCode", "elementCode", "wildCardGlobalRatio")
     wildCardGlobal$areaCode = countryCode
@@ -154,22 +170,18 @@ getShares = function(countryCode, itemCode, conn){
                          value.var = "ratio")
     castedFinal
 }
-(shares = getShares(testCountryCode, testItemCode, conn))
-
-
-## Need to check how the wild card work, we can already observe wild
-## card for year but specific country, and also global item wild card.
-
-
-dbGetQuery(conn, "SELECT * FROM aupus_ratios WHERE area = 0 AND YR = 0")
-dbGetQuery(conn, "SELECT * FROM aupus_ratios WHERE area = 79 AND item = 15")
+ratio = getRatios(testCountryCode, testItemCode, conn)
+save(ratio, file = "ratio.RData")
 
 
 ## Should also merge the input, by we will not do this for now for the
 ## primary commodity.
 mergedAupus =
-    merge(rawAupus, shares, by = c("areaCode", "itemCode", "Year"),
-          all = TRUE)
+    Reduce(f = function(x, y){
+        merge(x, y, by = c("areaCode", "itemCode", "Year"), all = TRUE)
+    },
+           x = list(rawAupus, input, ratio))
+
 
 
 
