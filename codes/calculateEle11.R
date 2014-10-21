7442c3177bf46aac71d28286ac89d851f1bbdec5
mkao006 <mkao006@gmail.com>
2014-10-21 17:49:16 +0200

    fixed missing argument and also added section for symbol

--- codes/calculateEle11.R
+++ codes/calculateEle11.R
@@ -1,11 +1,16 @@
-calculateEle11 = function(element11Num, element161Num, data){
+calculateEle11 = function(element11Num, element11Symb,
+    element161Num, data){
     setnames(data,
-             old = c(element11Num, element161Num),
-             new = c("element11Num", "element161Num"))
+             old = c(element11Num, element11Symb, element161Num),
+             new = c("element11Num", "element11Symb", "element161Num"))
     ## The list is contained in appendix A
     data[itemCode %in% c(2:10, 13, 19:22, 25:28, 30, 57),
          element11Num := trendOnce(element161Num),
          by = c("areaCode", "itemCode", "Year")]
-    setnames(new = c(element11Num, element161Num),
-             old = c("element11Num", "element161Num"))    
+    ## data[itemCode %in% c(2:10, 13, 19:22, 25:28, 30, 57),
+    ##      element11Symb := "C",
+    ##      by = c("areaCode", "itemCode", "Year")]    
+    setnames(data,
+             new = c(element11Num, element11Symb, element161Num),
+             old = c("element11Num", "element11Symb", "element161Num"))
 }
