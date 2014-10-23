606c11749cb89495516514595e29b1c6d265b24c
mkao006 <mkao006@gmail.com>
2014-10-23 19:13:04 +0200

    translated from test

--- codes/calculateEle58.R
+++ codes/calculateEle58.R
@@ -1,1 +1,16 @@
+calculateEle58 = function(element58Num, element58Symb, data){
+    setnames(data,
+             old = c(element58Num, element58Symb),
+             new = c("element58Num", "element58Symb"))
+    calcValue = data[itemCode == 3158, element58Num] +
+        data[itemCode == 3158, element58Num]
+    if(length(calcValue) != 0){
+        data[itemType == 57, element58Num := calcValue]
+        data[itemType == 57 & element58Symb != "M",
+             elementSymb := "C"]
+    }
+    setnames(data,
+             new = c(element58Num, element58Symb),
+             old = c("element58Num", "element58Symb"))
+}
 
