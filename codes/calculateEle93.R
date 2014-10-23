bb02c0f69645a3ed54802209ed4288f2d3768d14
mkao006 <mkao006@gmail.com>
2014-10-23 19:13:20 +0200

    corrected naming of symbol

--- codes/calculateEle93.R
+++ codes/calculateEle93.R
@@ -1,8 +1,10 @@
 calculateEle93 = function(element91Num, element92Num,
     element93Num, element93Symb, data){
     setnames(data,
-             old = c(element91Num, element92Num, element93Num),
-             new = c("element91Num", "element92Num", "element93Num"))
+             old = c(element91Num, element92Num, element93Num,
+                 element93Symb),
+             new = c("element91Num", "element92Num", "element93Num",
+                     "element93Symb"))
     ## Calculate value
     data[!itemType %in% c(42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52) &
          !is.na(element91Num) & !is.na(element92Num),
@@ -20,6 +22,8 @@ calculateEle93 = function(element91Num, element92Num,
          element93Symb := "M"]    
     
     setnames(data,
-             new = c(element91Num, element92Num, element93Num),
-             old = c("element91Num", "element92Num", "element93Num"))    
+             new = c(element91Num, element92Num, element93Num,
+                     element93Symb),
+             old = c("element91Num", "element92Num", "element93Num",
+                     "element93Symb"))
 }
