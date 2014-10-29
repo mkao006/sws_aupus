437e975fa8ad2232ff3bc461815baaf4a9c706ee
mkao006 <mkao006@gmail.com>
2014-10-29 16:23:47 +0100

    allowing multiple standardization

--- codes/standardizeCommodityNetwork.R
+++ codes/standardizeCommodityNetwork.R
@@ -1,20 +1,48 @@
 standardizeCommodityNetwork = function(shares, aupus, extractionRate,
-    standardizeElement, commodity){
-    print(commodity)
+    standardizeElement, commodity, plot = FALSE){
+    ## print(commodity)
+    ## print(str(shares))
     graph = constructGraph(shares = shares, aupus = aupus,
         extractionRate = extractionRate,
         standardizeElement = standardizeElement)
-    sub.graph = findConnectedGraph(graph, commodity = commodity)
-
-    ## plot(sub.graph, vertex.size = 8, edge.arrow.size = 0.5,
-    ##      vertex.label = paste0(V(sub.graph)$name, "\n(",
-    ##          V(sub.graph)$standardizeElement, ")"))
-    ## print(sub.graph)
-    while(length(E(sub.graph)) > 0){
-        workingNode =
-            names(which(degree(sub.graph, mode = "in") == 0))
-        sub.graph = standardizeNode(sub.graph, workingNode)
+    if(length(E(graph)) > 0){
+        sub.graph = findConnectedGraph(graph, commodity = commodity)
+        if(plot)
+            plot(sub.graph, vertex.size = 8, edge.arrow.size = 0.5,
+                 vertex.label = paste0(V(sub.graph)$name, "\n(",
+                     V(sub.graph)$standardizeElement, ")"))
+        
+        intermediateStandardization = c()
+        while(length(E(sub.graph)) > 0){
+            workingNode =
+                names(which(degree(sub.graph, mode = "in") == 0 &
+                                degree(sub.graph, mode = "out") > 0))
+            standardize = standardizeNode(sub.graph, workingNode)
+            sub.graph =
+                standardize$standardizedGraph
+            if(plot)
+                plot(sub.graph, vertex.size = 8, edge.arrow.size = 0.5,
+                     vertex.label = paste0(V(sub.graph)$name, "\n(",
+                         V(sub.graph)$standardizeElement, ")"))
+            intermediateStandardization =
+                c(intermediateStandardization,
+                  standardize$intermediateValues)
+        }
+        gc()
+        terminalValue = V(sub.graph)$standardizeElement
+        names(terminalValue) = V(sub.graph)$name
+        fullStandardization =
+            c(terminalValue, intermediateStandardization)
+        standardizedValue = as.numeric(rep(NA, length(commodity)))
+        names(standardizedValue) = commodity
+        standardizedItem =
+            intersect(names(standardizedValue),
+                      names(fullStandardization))
+        standardizedValue[standardizedItem] =
+            fullStandardization[standardizedItem]
+    } else {
+        standardizedValue = as.numeric(rep(NA, length(commodity)))
+        names(standardizedValue) = commodity
     }
-    gc()
-    V(sub.graph)$standardizeElement
+    standardizedValue
 }
