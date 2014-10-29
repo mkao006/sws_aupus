4ffc3e051e757ea874f3dae9158275978338f9df
mkao006 <mkao006@gmail.com>
2014-10-29 16:22:20 +0100

    allowing for multiple commodities

--- codes/findConnectedGraph.R
+++ codes/findConnectedGraph.R
@@ -1,24 +1,12 @@
 findConnectedGraph = function(graph, commodity){
-    ## print(commodity)
-    ## print(V(graph))
-    ## print(E(graph))
-    ## print(commodity)
-    if(commodity %in% V(graph)$name){
-        dist = shortest.paths(graph = graph,
-            v = V(graph)[commodity], mode = "in")
-        ## print(dist)
-        ## print(V(graph)[colnames(dist)[is.finite(dist)]])
-        sub.graph = induced.subgraph(graph,
-            V(graph)[colnames(dist)[is.finite(dist)]])
-    } else {
-        artificialEdge =
-            data.frame(commodity = commodity, artifical = "000")
-        artificialNodes =
-            data.frame(commodity = c(commodity, "000"),
-                       standardizeElement = 0)
-        sub.graph = graph.data.frame(d = artificialEdge,
-                                     vertices = artificialNodes)
-        sub.graph = sub.graph - vertices("000")
-    }
-    sub.graph
+    dist = shortest.paths(graph = graph, v = V(graph)[commodity],
+        mode = "in")
+    if(any(!commodity %in% V(graph)$name))
+       stop("Commodity not found in graph")
+    connectedNodes =
+        unique(unlist(sapply(apply(X = dist, MARGIN = 1,
+                                   FUN = function(x){
+                                       which(is.finite(x))
+                                   }), names)))
+    induced.subgraph(graph, vids = V(graph)[connectedNodes])
 }
