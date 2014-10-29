3c0795d14862be9977ec7793a708345965499022
mkao006 <mkao006@gmail.com>
2014-10-29 17:36:53 +0100

    finalize trade standardization element66 and 96, and moved data query to get_data.R

--- tests/aupus_test.R
+++ tests/aupus_test.R
@@ -1,64 +1,7 @@
-# Load the libraries
+## source("get_data.R")
 library(igraph)
 library(reshape2)
-library(RJDBC)
 library(data.table)
-library(FAOSTAT)
-library(faoswsUtil)
-options(java.parameters = "-Xmx3000m")
-lapply(dir("../codes/", full.names = TRUE), FUN = source)
-
-
-## Connect to the database
-drv = JDBC(driverClass = "oracle.jdbc.driver.OracleDriver",
-    classPath = "~/ojdbc14.jar")
-conn = dbConnect(drv, "jdbc:oracle:thin:@lprdbwo1:3310:fstp",
-    user = "demo", password = "demo")
-
-swsItemTable = data.table(dbGetQuery(conn,
-    "SELECT item, name_e, item_typ FROM item"))
-setnames(swsItemTable,
-         old = c("ITEM", "NAME_E", "ITEM_TYP"),
-         new = c("itemCode", "itemName", "itemType"))
-setkeyv(swsItemTable, "itemCode")
-save(swsItemTable, file = "swsItemTable.RData")
-
-## Test of Germany
-testCountryCode = 100
-testItemCode = swsItemTable[swsItemTable$GRP_IND == "D", "ITEM"]
-aupusElements = c(11, 21, 31, 41, 51, 58, 61, 62, 66, 71, 91, 92, 95,
-    96, 101, 111, 121, 131, 141, 144, 151, 161, 171, 174, 261, 274, 281,
-    284, 541, 546)
-
-## Load aupus data
-aupus = getAupusData(testCountryCode,  conn)
-for(i in grep("NUM", colnames(aupus), value = TRUE)){
-    remove0M(data = aupus, value = i, flag = gsub("NUM", "SYMB", i),
-             naFlag = "M")
-}
-save(aupus, file = "aupusData.RData")
-
-population = getPopulation(testCountryCode, conn)
-save(population, file = "population.RData")
-
-## Get input data
-input = getInputFromProcess(testCountryCode, conn)
-remove0M(input, value = "NUM_INPUT", flag = "SYMB_INPUT")
-save(input, file = "input.RData")
-
-## Get ratio data
-ratio = getRatio(testCountryCode, conn)
-save(ratio, file = "ratio.RData")
-share.lst = getShare(testCountryCode, conn)
-share = mergeShare(share.lst)
-save(share, file = "share.RData")
-balanceElement = getBalanceElement(testCountryCode, conn)
-save(balanceElement, file = "balanceElement.RData")
-
-
-
-## Should also merge the input, by we will not do this for now for the
-## primary commodity.
 load("swsItemTable.RData")
 load("aupusData.RData")
 load("population.RData")
@@ -68,11 +11,9 @@ load("share.RData")
 load("balanceElement.RData")
 
 
-
+## Prepare the aupus data
 aupus = merge(aupus, swsItemTable, all.x = TRUE)
 setkeyv(aupus, c("areaCode", "itemCode", "Year"))
-
-
 aupusRatio = appendRatio(aupus, ratio, TRUE)
 aupusRatioBalanceElement =
     appendBalanceElement(aupusRatio, balanceElement, TRUE)
@@ -83,9 +24,6 @@ aggregatedInput = calculateTotalInput(updatedInput)
 aupusFinal = merge(aupusRatioBalanceElement, aggregatedInput,
     all.x = TRUE)
 setkeyv(aupusFinal, c("areaCode", "itemCode", "Year"))
-## aupusRatioInput = merge(aupusRatio, aggregatedInput, all.x = TRUE)
-## aupusFinal = merge(aupusRatioInput, population, all.x = TRUE)
-## setkeyv(aupusFinal, c("areaCode", "itemCode", "Year"))
 
 
 ## NOTE (Michael): There itemCodes are not in the ratio table and thus
@@ -142,34 +80,10 @@ calculateEle63(element61Num = "NUM_61",  element62Num = "NUM_62",
 
 
 ## Element 66
-
-
-
-## aupusFinal = merge(aupusRatio, aggregatedInput, all.x = TRUE)
-## setkeyv(aupusFinal, c("areaCode", "itemCode", "Year"))
-## test.graph =
-##     constructGraph(share[Year == 2010, ],
-##                    aupusFinal[Year == 2010, ], "NUM_41", "NUM_61", TRUE)
-## standardizeCommodityNetwork(share[Year == 2010, ],
-##                             aupusFinal[Year == 2010, ],
-##                             "NUM_41", "NUM_61", c("28", "29"))
-## V(standardizeNode(standardizeNode(test.graph, "29"), "35"))["28"]$standardizeElement
-## V(standardizeNode(test.graph, c("29", "35")))["28"]$standardizeElement
-
-## NOTE (Michael): Now can also recode the function to standarize by year.
-## Element 66
-## system.time(
-##     {
-##         calculateEle6696(data = aupusFinal, shares = share,
-##                          element41Num = "NUM_41", element61Num = "NUM_61",
-##                          element66Num = "NUM_66", element91Num = "NUM_91",
-##                          element96Num = "NUM_96")
-##     })
-
-
+calculateEle66(element41Num = "NUM_41", element61Num = "NUM_61",
+               element66Num = "NUM_66", data = aupusFinal, share = share)
 
 ## Element71
-
 calculateEle71(element71Num = "NUM_71", element71Symb = "SYMB_71",
                element51Num = "NUM_51", element61Num = "NUM_61",
                element91Num = "NUM_91", element101Num = "NUM_101",
@@ -182,6 +96,10 @@ calculateEle71(element71Num = "NUM_71", element71Symb = "SYMB_71",
 calculateEle93(element91Num = "NUM_91", element92Num = "NUM_92",
                element93Num = "NUM_93", element93Symb = "SYMB_93",
                data = aupusFinal)
+
+## Element 96
+calculateEle96(element41Num = "NUM_41", element91Num = "NUM_91",
+               element96Num = "NUM_96", data = aupusFinal, share = share)
 
 
 ## Calculate total supply
