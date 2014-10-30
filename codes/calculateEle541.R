e7309e6de5babf73c10a2669758ec95f3c78c54e
mkao006 <mkao006@gmail.com>
2014-10-30 14:39:00 +0100

    merged assignment of values and symbols, and also replaced 0M with NAM

--- codes/calculateEle541.R
+++ codes/calculateEle541.R
@@ -1,12 +1,14 @@
 calculateEle541 = function(element541Num, element541Symb,
     element542Num, element543Num, element544Num, element545Num, data){
 
+    ## If any of these elements were missing, then assign the column
+    ## with NA
     finalDemandElements =
         c(element542Num, element543Num, element544Num, element545Num)
     if(any(!finalDemandElements %in% colnames(data)))
         data[, `:=`(finalDemandElements[which(!finalDemandElements %in%
                                               colnames(data))],
-                    NA)]
+                    as.numeric(NA))]
     
     setnames(data,
              old = c(element541Num, element541Symb, element542Num,
@@ -17,13 +19,13 @@ calculateEle541 = function(element541Num, element541Symb,
     data[, numberOfMissingElements :=
              numberOfMissingElement(element542Num, element543Num,
                                     element544Num, element545Num)]
-    data[, element541Num := element542Num + element543Num +
-             element544Num + element545Num]
-    data[numberOfMissingElements == 4, element541Num := 0]
-
-    data[!is.na(element541Num) & element541Symb == "M",
-         element541Symb := "C"]
-    data[element541Num == 0, element541Symb := "M"]    
+    data[, `:=`(c("element541Num", "element541Symb"),
+                list(element542Num + element543Num + element544Num +
+                     element545Num, "C"))]
+    data[numberOfMissingElements == 4,
+         `:=`(c("element541Num", "element541Symb"),
+              list(NA, "M"))]
+    
     data[, numberOfMissingElements := NULL]
     
     setnames(data,
