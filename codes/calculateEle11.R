5855ffd19498be4b1f5b2c08fc71197f83fa9368
mkao006 <mkao006@gmail.com>
2014-10-30 12:21:06 +0100

    Remove redudant code

--- codes/calculateEle11.R
+++ codes/calculateEle11.R
@@ -9,10 +9,6 @@ calculateEle11 = function(element11Num, element11Symb,
     data[itemCode %in% c(2:10, 13, 19:22, 25:28, 30, 57),
          element11Num := trendOnce(element161Num),
          by = c("areaCode", "itemCode", "Year")]
-    ## data[itemCode %in% c(2:10, 13, 19:22, 25:28, 30, 57) &
-    ##      !is.na(element11Num) & element11Symb == "M",
-    ##      element11Symb := "C",
-    ##      by = c("areaCode", "itemCode", "Year")]
     data[itemCode %in% c(2:10, 13, 19:22, 25:28, 30, 57),
          element11Symb := transferSymb(trendOnce(element161Symb)),
          by = c("areaCode", "itemCode", "Year")]    
