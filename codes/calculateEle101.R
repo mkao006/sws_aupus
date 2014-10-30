4703cf8351c9a5cdee1c3f0795a065d7c446de8c
mkao006 <mkao006@gmail.com>
2014-10-30 14:11:31 +0100

    merged assignment of value and symbol

--- codes/calculateEle101.R
+++ codes/calculateEle101.R
@@ -1,15 +1,12 @@
 calculateEle101 = function(element101Num, element101Symb,
     ratio101Num, stotal, data){
-    ## Assumes total is calculated already.
-    ## NOTE (Michael): how to calculate total supply?
     setnames(data,
              old = c(element101Num, element101Symb, ratio101Num, stotal),
              new = c("element101Num", "element101Symb",
                  "ratio101Num", "stotal"))
     data[!is.na(ratio101Num),
-         element101Num := ratio101Num * stotal/100]
-    data[!is.na(element101Num) & element101Symb == "M",
-         element101Symb := "C"]
+         `:=`(c("element101Num", "element101Symb"),
+              list(ratio101Num * stotal/100, "C"))]
     setnames(data,
              new = c(element101Num, element101Symb, ratio101Num, stotal),
              old = c("element101Num", "element101Symb",
