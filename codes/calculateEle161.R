51346eb391aec61fda4982c68284d29dee32b46b
mkao006 <mkao006@gmail.com>
2014-10-30 14:29:30 +0100

    merged assignment of value and symbol

--- codes/calculateEle161.R
+++ codes/calculateEle161.R
@@ -5,12 +5,14 @@ calculateEle161 = function(element161Num, element161Symb,
                  element11Num, element71Num),
              new = c("element161Num", "element161Symb", "element11Num",
                  "element71Num"))
-    data[itemType == 57, element161Num := element11Num + element71Num]
-    data[itemType == 57 & !is.na(element161Num) & element161Symb == "M",
-         element161Symb := "C"]
+    data[itemType == 57,
+         `:=`(c("element161Num", "element161Symb"),
+              list(element11Num + element71Num, "C"))]
+
     ## NOTE (Michael): It just says that the next year is forced to be
-    ## processed if it is of type trade and also element 11 was
-    ## already calculated for the following year.
+    ##                 processed if it is of type trade and also
+    ##                 element 11 was already calculated for the
+    ##                 following year.
     ##
     ## data[itemType %in% c(2:13, 19:22, 25:30, 39), ]
     setnames(data,
