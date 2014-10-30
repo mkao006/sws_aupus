db7e3d96284c9eb73aaddab669b0302a1a880327
mkao006 <mkao006@gmail.com>
2014-10-30 12:16:31 +0100

    corrected symbol

--- codes/calculateEle11.R
+++ codes/calculateEle11.R
@@ -1,17 +1,24 @@
 calculateEle11 = function(element11Num, element11Symb,
-    element161Num, data){
+    element161Num, element161Symb, data){
     setnames(data,
-             old = c(element11Num, element11Symb, element161Num),
-             new = c("element11Num", "element11Symb", "element161Num"))
+             old = c(element11Num, element11Symb, element161Num,
+                     element161Symb),
+             new = c("element11Num", "element11Symb", "element161Num",
+                     "element161Symb"))
     ## The list is contained in appendix A
     data[itemCode %in% c(2:10, 13, 19:22, 25:28, 30, 57),
          element11Num := trendOnce(element161Num),
          by = c("areaCode", "itemCode", "Year")]
-    data[itemCode %in% c(2:10, 13, 19:22, 25:28, 30, 57) &
-         !is.na(element11Num) & element11Symb == "M",
-         element11Symb := "C",
-         by = c("areaCode", "itemCode", "Year")]
+    ## data[itemCode %in% c(2:10, 13, 19:22, 25:28, 30, 57) &
+    ##      !is.na(element11Num) & element11Symb == "M",
+    ##      element11Symb := "C",
+    ##      by = c("areaCode", "itemCode", "Year")]
+    data[itemCode %in% c(2:10, 13, 19:22, 25:28, 30, 57),
+         element11Symb := transferSymb(trendOnce(element161Symb)),
+         by = c("areaCode", "itemCode", "Year")]    
     setnames(data,
-             new = c(element11Num, element11Symb, element161Num),
-             old = c("element11Num", "element11Symb", "element161Num"))
+             new = c(element11Num, element11Symb, element161Num,
+                     element161Symb),
+             old = c("element11Num", "element11Symb", "element161Num",
+                     "element161Symb"))
 }
