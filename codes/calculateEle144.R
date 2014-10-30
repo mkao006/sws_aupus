c036b24953a7cb0a2c581f8ee0a963b34abf2bfc
mkao006 <mkao006@gmail.com>
2014-10-30 14:26:43 +0100

    remove condition for assignment of symbol

--- codes/calculateEle144.R
+++ codes/calculateEle144.R
@@ -5,14 +5,12 @@ calculateEle144 = function(element144Num, element144Symb,
                  element141Num, population),
              new = c("element144Num", "element144Symb",
                  "element141Num", "population"))
-    ## Assumes total consumption (element141Num) has already been
-    ## calculated.
+
     data[itemType %in% c(46, 47, 48, 51, 52, 58, 59, 60, 61),
          element144Num := element141Num/population * 1000]
     data[!itemType %in% c(46, 47, 48, 51, 52, 58, 59, 60, 61),
          element144Num := element141Num/population]
-    data[!is.na(element144Num) & element144Symb == "M",
-         element144Symb := "C"]
+    data[, element144Symb := "C"]
     
     setnames(data,
              new = c(element144Num, element144Symb,
