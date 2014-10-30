278dd3c93c193df2e4b9d80e7209c170cf39d769
mkao006 <mkao006@gmail.com>
2014-10-30 14:30:13 +0100

    merged assignment of value and symbol

--- codes/calculateEle171.R
+++ codes/calculateEle171.R
@@ -7,11 +7,12 @@ calculateEle171 = function(element171Num, element171Symb, element101Num,
              new = c("element171Num", "element171Symb", "element101Num",
                  "element121Num",
                  "element131Num", "element141Num", "element151Num"))
-    data[itemCode == 57, element171Num := element101Num +
-             element121Num + element131Num + element141Num +
-                 element151Num]
-    data[itemCode == 57 & !is.na(element171Num) & element171Symb == "M",
-         element171Symb := "C"]
+
+    data[itemCode == 57,
+         `:=`(c("element171Num", "element171Symb"),
+              list(element101Num + element121Num + element131Num +
+                   element141Num + element151Num, "C"))]
+
     setnames(data,
              new = c(element171Num, element171Symb, element101Num,
                  element121Num,
