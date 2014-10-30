d36f7bd9351dda08d8011e41ddc99c6b79f47055
mkao006 <mkao006@gmail.com>
2014-10-30 13:58:21 +0100

    merged assignment of value and symbol and also corrected item3158 to 3159

--- codes/calculateEle58.R
+++ codes/calculateEle58.R
@@ -2,13 +2,12 @@ calculateEle58 = function(element58Num, element58Symb, data){
     setnames(data,
              old = c(element58Num, element58Symb),
              new = c("element58Num", "element58Symb"))
-    calcValue = data[itemCode == 3158, element58Num] +
-        data[itemCode == 3158, element58Num]
-    if(length(calcValue) != 0){
-        data[itemType == 57, element58Num := calcValue]
-        data[itemType == 57 & element58Symb != "M",
-             elementSymb := "C"]
-    }
+    calcValue = unlist(data[itemCode == 3158, element58Num]) +
+        unlist(data[itemCode == 3159, element58Num])
+    if(length(calcValue) != 0)
+        data[itemType == 57,
+             `:=`(c("element58Num", "element58Symb"),
+                  list(calcValue, "C"))]
     setnames(data,
              new = c(element58Num, element58Symb),
              old = c("element58Num", "element58Symb"))
