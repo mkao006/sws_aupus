a191480918a284aca6e37e75e89eac621e23d27c
mkao006 <mkao006@gmail.com>
2014-10-30 14:47:24 +0100

    changed addition with + to rowSums for missing values and also merged the assignment for values and symbol

--- codes/calculateEle546.R
+++ codes/calculateEle546.R
@@ -8,13 +8,15 @@ calculateEle546 = function(element546Num, element546Symb, element541Num,
     data[, numberOfMissingElements :=
              numberOfMissingElement(element541Num, element151Num,
                                     element191Num)]
-    data[, element541Num := element541Num + element151Num +
-             element191Num]
-    data[numberOfMissingElements == 0, element541Num := 0]
-    data[numberOfMissingElements != 0 & !is.na(element541Num),
-         element541Symb := "C"]
-    data[numberOfMissingElements == 0, element541Symb := "M"]
+    data[, `:=`(c("element546Num", "element546Symb"),
+                list(rowSums(.SD[, list(element541Num, element151Num,
+                                        element191Num)], na.rm = TRUE),
+                     "C"))]
+    data[numberOfMissingElements == 3,
+         `:=`(c("element546Num", "element546Symb"), list(NA, "M"))]
+    
     data[, numberOfMissingElements := NULL]
+
     setnames(data,
              new = c(element546Num, element546Symb, element541Num,
                  element151Num, element191Num),
