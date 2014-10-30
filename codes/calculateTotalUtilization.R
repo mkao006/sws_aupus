06a62e2786911881582e7a1e942bc7029fbd0145
mkao006 <mkao006@gmail.com>
2014-10-30 14:50:53 +0100

    changed addition with + to rowSums for missing values

---
+++ codes/calculateTotalUtilization.R
@@ -0,0 +1,43 @@
+calculateTotalUtilization = function(element91Num, element95Num,
+    element96Num, element101Num, element111Num, element121Num,
+    element131Num, element141Num, element161Num, element546Num,
+    element151Num, data){
+    setnames(data,
+             old = c(element91Num, element95Num, element96Num,
+                 element101Num, element111Num, element121Num,
+                 element131Num, element141Num, element161Num,
+                 element546Num, element151Num),
+             new = c("element91Num", "element95Num", "element96Num",
+                 "element101Num", "element111Num", "element121Num",
+                 "element131Num", "element141Num", "element161Num",
+                 "element546Num", "element151Num"))
+
+    data[!itemType %in% 53,
+         TOTAL_UTILIZATION :=
+             rowSums(.SD[, list(element91Num, element95Num,
+                                element96Num, element101Num,
+                                element111Num, element121Num,
+                                element131Num, element141Num,
+                                element161Num, element546Num)],
+                     na.rm = TRUE)]
+
+    data[itemType %in% 53,
+         TOTAL_UTILIZATION :=
+             rowSums(.SD[, list(element91Num, element95Num,
+                                element96Num, element101Num,
+                                element111Num + element121Num,
+                                element131Num, element141Num,
+                                element151Num, element161Num,
+                                element546Num)],
+                     na.rm = TRUE)]
+
+    setnames(data,
+             new = c(element91Num, element95Num, element96Num,
+                 element101Num, element111Num, element121Num,
+                 element131Num, element141Num, element161Num,
+                 element546Num, element151Num),
+             old = c("element91Num", "element95Num", "element96Num",
+                 "element101Num", "element111Num", "element121Num",
+                 "element131Num", "element141Num", "element161Num",
+                 "element546Num", "element151Num"))
+}
