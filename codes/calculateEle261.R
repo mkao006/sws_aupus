6a95d8d6db1030c7f8ed4e3e4afdfec54a706f3e
mkao006 <mkao006@gmail.com>
2014-10-30 14:31:44 +0100

    merged assignment of value and symbol

--- codes/calculateEle261.R
+++ codes/calculateEle261.R
@@ -5,9 +5,8 @@ calculateEle261 = function(element261Num, element261Symb, ratio261Num,
                  element141Num),
              new = c("element261Num", "element261Symb", "ratio261Num",
                  "element141Num"))
-    data[, element261Num := ratio261Num * element141Num/100]
-    data[!is.na(element261Num) & element261Symb == "M",
-         element261Symb := "C"]
+    data[, `:=`(c("element261Num", "element261Symb"),
+                list(ratio261Num * element141Num/100, "C"))]
     setnames(data,
              new = c(element261Num, element261Symb, ratio261Num,
                  element141Num),
