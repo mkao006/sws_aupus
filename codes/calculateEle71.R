2cdf1e784f83a9d25a6121cae86e9996062b54b2
mkao006 <mkao006@gmail.com>
2014-10-30 14:06:59 +0100

    merged assignment of value and symbol

--- codes/calculateEle71.R
+++ codes/calculateEle71.R
@@ -14,15 +14,14 @@ calculateEle71 = function(element71Num, element71Symb,
                  "element131Num", "element141Num", "element151Num",
                  "element161Num"))
     data[itemType == 58,
-         element71Num := element51Num + element61Num -
-             element91Num - element101Num - element121Num -
-                 element131Num - element141Num - element151Num]    
+         `:=`(c("element71Num", "element71Symb"),
+              list(element51Num + element61Num -
+                   element91Num - element101Num - element121Num -
+                   element131Num - element141Num - element151Num, "C"))]
     data[itemType %in% c(59, 60, 61),
-         element71Num := element161Num - element101Num]
+         `:=`(c("element71Num", "element71Symb"),
+              list(element161Num - element101Num, "C"))]
     
-    data[itemType == 58 & !is.na(element71Num), element71Symb := "C"]    
-    data[itemType %in% c(59, 60, 61) & !is.na(element71Num),
-         element71Symb := "C"]    
     ## NOTE (Michael): Ignoring element 57
     setnames(data,
              new = c(element71Num, element71Symb,
