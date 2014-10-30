96aff3f5cd47063a8c093db8a78eaf4cd7539613
mkao006 <mkao006@gmail.com>
2014-10-30 12:29:40 +0100

    merged assignment of value and symbol

--- codes/calculateEle31.R
+++ codes/calculateEle31.R
@@ -3,28 +3,18 @@ calculateEle31 = function(element31Num, element31Symb, inputNum,
     setnames(data,
              new = c("element31Num", "element31Symb", "inputNum"),
              old = c(element31Num, element31Symb, inputNum))
-    ## NOTE (Michael): These are assumed from the names in the Annex 3   
+    ## NOTE (Michael): These are assumed from the names in Annex 3
     data[itemType %in% c(3, 10, 13, 15, 16, 22, 26, 27, 28, 3,
                          32, 33, 5, 6, 7, 8, 9) &
          (is.calculated(element31Symb) | element31Symb == "M") &
          !is.na(inputNum),
-         element31Num := inputNum]
-    data[itemType %in% c(3, 10, 13, 15, 16, 22, 26, 27, 28, 3,
-                         32, 33, 5, 6, 7, 8, 9) &
-         (is.calculated(element31Symb) | element31Symb == "M") &
-         !is.na(inputNum),                             
-         element31Symb := "C"]    
-    
+         `:=`(c("element31Num", "element31Symb"),
+              list(inputNum, "C"))]
     data[itemType %in% c(3, 10, 13, 15, 16, 22, 26, 27, 28, 3,
                          32, 33, 5, 6, 7, 8, 9) &
          (is.calculated(element31Symb) | element31Symb == "M") &
          is.na(inputNum),
-         element31Num := 0]
-    data[itemType %in% c(3, 10, 13, 15, 16, 22, 26, 27, 28, 3,
-                         32, 33, 5, 6, 7, 8, 9) &
-         (is.calculated(element31Symb) | element31Symb == "M") &
-         is.na(inputNum),
-         element31Symb := "M"]
+         `:=`(c("element31Num", "element31Symb"), list(0, "M"))]
     setnames(data,
              old = c("element31Num", "element31Symb", "inputNum"),
              new = c(element31Num, element31Symb, inputNum))    
