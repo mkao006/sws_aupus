2f47a776dfa0715a360bcd1205002d9261bec8ed
mkao006 <mkao006@gmail.com>
2014-11-12 17:58:52 +0100

    moved filled balance to a new script, and added roxygen and replacement index

--- codes/calculateBalance.R
+++ codes/calculateBalance.R
@@ -1,6 +1,33 @@
+##' The function will calculate the balance and fill in the element to
+##' be balanced.
+##'
+##' The function calculates the balance based on suppply and
+##' utilization and fill in the balancing element.
+##'
+##' @param supply The column corresponding to calculated total supply.
+##' @param utilization The column correspoding to calculated total
+##' utilization.
+##' @param element161Num The column corresponding to value of element
+##' 161.
+##' @param element171Num The column corresponding to value of element
+##' 171.
+##' @param element181Num The column corresponding to value of element
+##' 181.
+##' @param element181Symb The column corresponding to symbol of element
+##' 181.
+##' @param balanceElement The column corresponding to the balancing
+##' element.
+##' @param itemTypeCol The column which identifies the item type of
+##' the commodity item.
+##' @param data The data
+##' @export
+##' 
+
 calculateBalance = function(supply, utilization, element161Num,
     element171Num, element181Num, element181Symb, balanceElement,
     itemTypeCol, data){
+    ## TODO (Michael): Need to check the replace column of this
+    ##                 function.
     setnames(data,
              old = c(supply, utilization, element161Num, element171Num,
                      element181Num, element181Symb, balanceElement),
@@ -20,54 +47,21 @@ calculateBalance = function(supply, utilization, element161Num,
 
     ## Reverse the value if balance element is 71.
     data[balanceElement == 71, BALANCE := -BALANCE]
-    
-    fillBalance = function(balanceValue, balanceNum, balanceSymb,
-        element181Num, element181Symb, data){
-        ## Replace if the balancing element is calculated or missing
-        ## and calculated balancing value is greater than zero
-        if(balanceNum == "NUM_181"){
-            balanceNum = element181Num
-            balanceSymb = element181Symb
-        }
-        
-        toReplace =
-            unlist(data[, balanceSymb, with = FALSE]) %in% c("M", "C") &
-            unlist(data[, balanceValue, with = FALSE]) > 0
-        toReplace[is.na(toReplace)] = FALSE
-        originalValue = unlist(data[, balanceNum, with = FALSE])
-        originalValue[toReplace] =
-            unlist(data[toReplace, balanceValue, with = FALSE])
-        originalSymb = unlist(data[, balanceSymb, with = FALSE]) 
-        originalSymb[toReplace] = "C"
-        discrepancyValue = unlist(data[, element181Num, with = FALSE])
-        discrepancyValue[toReplace] = 0
-        discrepancySymb = unlist(data[, element181Symb, with = FALSE])
-        discrepancySymb[toReplace] = "M"
-
-        ## If neither is satisfied, then set the balancing element to
-        ## 0 and the calculated balancing value to statistical
-        ## discrepancy.
-        originalValue[!toReplace] = 0
-        originalSymb[!toReplace] = "M"
-        discrepancyValue[!toReplace] =
-            unlist(data[!toReplace, balanceValue, with = FALSE])
-        discrepancySymb[!toReplace] = "C"
-        list(originalValue, originalSymb, discrepancyValue,
-             discrepancySymb)
-    }
 
     data[!is.na(balanceElement),
          `:=`(c(paste0("NUM_", .BY[[1]]), paste0("SYMB_", .BY[[1]]),
-                "element181Num", "element181Symb"),
+                "element181Num", "element181Symb", "replaced"),
               fillBalance("BALANCE", paste0("NUM_", .BY[[1]]),
                           paste0("SYMB_", .BY[[1]]),
                           "element181Num", "element181Symb", .SD)),
          by = "balanceElement"]
+    replaceIndex1 = which(unlist(data[, replaced]))
+    ## data[, replaced := NULL]
     setnames(data,
              new = c(supply, utilization, element161Num, element171Num,
                      element181Num, element181Symb, balanceElement),
              old = c("supply", "utilization", "element161Num",
                  "element171Num", "element181Num", "element181Symb",
                  "balanceElement"))    
-
+    replaceIndex1
 }
