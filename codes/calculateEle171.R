fc7a65149c68b0e9e8ba1e71f29c03921016ad01
mkao006 <mkao006@gmail.com>
2014-11-12 18:01:13 +0100

    added roxygen and improved the calculation with '+' to rowSums

--- codes/calculateEle171.R
+++ codes/calculateEle171.R
@@ -1,3 +1,24 @@
+##' The function calculates element 171 (consumption sugar)
+##'
+##' @param element171Num The column corresponding to value of element
+##' 171.
+##' @param element171Symb The column corresponding to symbol of element
+##' 171.
+##' @param element101Num The column corresponding to value of element
+##' 101.
+##' @param element121Num The column corresponding to value of element
+##' 121.
+##' @param element131Num The column corresponding to value of element
+##' 131.
+##' @param element141Num The column corresponding to value of element
+##' 141.
+##' @param element151Num The column corresponding to value of element
+##' 151.
+##' @param data The data
+##' @export
+##' 
+
+
 calculateEle171 = function(element171Num, element171Symb, element101Num,
     element121Num, element131Num, element141Num, element151Num, data){
     setnames(data,
@@ -7,14 +28,16 @@ calculateEle171 = function(element171Num, element171Symb, element101Num,
              new = c("element171Num", "element171Symb", "element101Num",
                  "element121Num",
                  "element131Num", "element141Num", "element151Num"))
-    replaceIndex = which(data[[key(data)[2]]] == 57 &
+    replaceIndex1 = which(data[[key(data)[2]]] == 57 &
                          replaceable(data$element171Symb))
-    print(replaceIndex)
-    data[replaceIndex,
+    data[replaceIndex1,
          `:=`(c("element171Num", "element171Symb"),
-              appendSymbol(element101Num + element121Num +
-                           element131Num + element141Num +
-                           element151Num, "C"))]
+              appendSymbol(rowSums(.SD[, list(element101Num,
+                                              element121Num,
+                                              element131Num,
+                                              element141Num,
+                                              element151Num)],
+                                   na.rm = TRUE), "C"))]
 
     setnames(data,
              new = c(element171Num, element171Symb, element101Num,
@@ -22,5 +45,6 @@ calculateEle171 = function(element171Num, element171Symb, element101Num,
                  element131Num, element141Num, element151Num),
              old = c("element171Num", "element171Symb", "element101Num",
                  "element121Num",
-                 "element131Num", "element141Num", "element151Num"))    
+                 "element131Num", "element141Num", "element151Num"))
+    replaceIndex1
 }
