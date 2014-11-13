914de53fb3edd12ba1d50bcde063d8178afc4ac1
mkao006 <mkao006@gmail.com>
2014-11-13 19:40:02 +0100

    First commit of the function to get item information data

---
+++ codes/getItemInfoData.R
@@ -0,0 +1,25 @@
+##' Function to get item information data
+##'
+##' The name and type of the commodity corresponding to the item code
+##' is returned.
+##'
+##' @export
+##' 
+
+getItemInfoData = function(){
+    itemCodeList =
+        GetCodeList(domain = "faostat_one",
+                    dataset = "FS1_SUA",
+                    dimension = "measuredItemFS")
+    itemCodeList[, startDate := as.Date(NULLtoNA(startDate))]
+    itemCodeList[, endDate := as.Date(NULLtoNA(endDate))]
+    setnames(itemCodeList,
+             old = c("code", "description", "type"),
+             new = c("measuredItemFS", "measuredItemNameFS",
+                     "measuredItemTypeFS"))
+    swsItemTable =
+        itemCodeList[, list(measuredItemFS, measuredItemNameFS,
+                            measuredItemTypeFS)]
+    setkeyv(swsItemTable, "measuredItemFS")
+    swsItemTable
+}
