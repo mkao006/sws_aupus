2f3667f36785e7b8d6c6a1847c2263ac0b9e25ec
mkao006 <mkao006@gmail.com>
2014-11-14 16:43:46 +0100

    added roxygen

--- codes/getInputFromProcessData.R
+++ codes/getInputFromProcessData.R
@@ -1,3 +1,14 @@
+##' This function extracts the input from processing data from the
+##' data base.
+##'
+##' @param database Whether to use the new or the old statistical
+##' working system.
+##' @param param The parameter file from getAupusParam
+##' @param conn The RJDBS connection to the old working system.
+##' @export
+##' 
+
+
 getInputFromProcessData = function(database = c("new", "old"), param,
     conn){
     database = match.arg(database)
