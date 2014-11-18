2b66e0fdc5d45bb0dfc73d5b8745aa9bb5f78dae
mkao006 <mkao006@gmail.com>
2014-11-18 18:35:52 +0100

    Changed graph

--- codes/findProcessingLevel.R
+++ codes/findProcessingLevel.R
@@ -7,20 +7,21 @@
 ##' @param plot Whether the graph should be plotted.
 ##'
 ##' @export
+##' 
 
 findProcessingLevel = function(edgeData, from, to, plot = FALSE){
     e = edgeData[, c(from, to), with = FALSE]
     v = unique(unlist(aupusEdges[, c(from, to), with = FALSE]))
     processingGraph = graph.data.frame(d = e, vertices = v, directed = TRUE)
-    
+    if(plot == TRUE)
+            plot(processingGraph, vertex.size = 8, edge.arrow.size = 0.5)    
     root = names(which(degree(processingGraph, mode = "in") == 0 &
                            degree(processingGraph, mode = "out") > 0))
 
     processingLevel =
         shortest.paths(processingGraph, v = V(processingGraph),
                        to = V(processingGraph)[c(root)], mode = "in")
-    if(plot == TRUE)
-        plot(processingGraph)
+
 
     ## Take the finite maximum level fro processing level
     finalLevels = apply(processingLevel, 1,
