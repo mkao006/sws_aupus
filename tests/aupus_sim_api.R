af6d62b5caf3a092e99864a440700d91571ef19a
mkao006 <mkao006@gmail.com>
2014-11-26 18:27:10 +0100

    finalize script, ready for r module development

--- tests/aupus_sim_api.R
+++ tests/aupus_sim_api.R
@@ -1,11 +1,23 @@
 ## Load libraries
 ## testCountryCode = 100
+## source("test_get_api_data.R")
+
 library(faoswsAupus)
 library(faoswsUtil)
 library(data.table)
 library(igraph)
-    
-source("test_get_api_data.R")
+
+FBSelements = c("Value_measuredElementFS_61", "Value_measuredElementFS_91",
+    "Value_measuredElementFS_111",
+    "Value_measuredElementFS_121", "Value_measuredElementFS_141")
+
+## Get the parameter
+param = getAupusParameter(areaCode = "100", assignGlobal = FALSE)
+
+## Get the data sets
+getAupusDataset()
+
+## This is a hack to fill in the missing columns
 missingColumns =
     c(paste0("Value_measuredElementFS_", c(541, 546)),
       paste0("flagFaostat_measuredElementFS_", c(541, 546)))
@@ -13,438 +25,55 @@ missingColumns =
 aupusData[, `:=`(c(missingColumns),
                  list(as.numeric(NA), as.numeric(NA), as.character(NA),
                       as.character(NA)))]
+   
+## Construct the aupus network representation
+aupusNetwork =
+    suaToNetworkRepresentation(extractionRateData = extractionRateData,
+                               shareData = shareData,
+                               inputData = inputData,
+                               ratioData = ratioData,
+                               balanceElementData = balanceElementData,
+                               itemInfoData = itemInfoData,
+                               populationData = populationData)
 
 
+## Run the aupus to update the data
+updatedAupusNetwork =
+    with(aupusNetwork,
+         Aupus(nodes = nodes, edges = edges,
+               from = param$keyNames$itemParentName,
+               to = param$keyNames$itemChildName))
 
 
-aupusFinal =
-    mergeAll(aupusData = aupusData, itemInfoData = itemInfoData,
-             balanceElementData = balanceElementData,
-             shareData = shareData,
-             inputData = inputData, param = param,
-             inputNum = "Value_input",
-             balanceElementNum = "balanceElement",
-             shares = "Value_share",
-             element131Num = "Value_measuredElementFS_131")
-
-## ## Replace Num col with value equivalent to its column
-## lapply(grep("NUM|RATIO", colnames(aupusSim), value = TRUE),
-##        FUN = function(x){
-##            num = as.numeric(gsub("[^0-9]", "", x))
-##            if(is.na(num))
-##                num = 100
-##            aupusSim[, `:=`(c(x), num)]
-##        }
-## )
-
-## ## resample symbol to replace missing flag
-## resampleSymb = function(symbs){
-##     print(str(symbs))
-##     if(!all(is.na(symbs))){
-##         missIndex = which(is.na(symbs))
-##         symbs[is.na(symbs)] =
-##             sample(unique(na.omit(symbs)), length(missIndex),
-##                    replace = TRUE)
-##     } else {
-##         symbs = rep("C", length(symbs))
-##     }
-##     symbs
-## }
-
-## lapply(grep("SYMB", colnames(aupusSim), value = TRUE),
-##        FUN = function(x){
-##            aupusSim[, `:=`(c(x),
-##                            resampleSymb(unlist(aupusSim[, x, with = FALSE])))]
-##        })
-
-## aupusSim[is.na(balanceElement),
-##          balanceElement := unique(na.omit(aupusSim$balanceElement))]
-
-## aupusCopy = copy(aupusSim)
-
-## compareFunction = function(replaceIndex, elementNum){
-##     tmp =
-##         cbind(aupusFinal[replaceIndex,
-##                        c("areaCode", "itemCode", "itemType", "Year",
-##                          paste0(c("NUM_", "SYMB_"), elementNum)),
-##                        with = FALSE],
-##               aupusCopy[replaceIndex,
-##                        paste0(c("NUM_", "SYMB_"), elementNum),
-##                        with = FALSE])
-##     print(tmp, nrow = NROW(tmp))
-## }
-
-## compareFunction = function(replaceIndex, elementNum){
-##     print(paste0("PASS", elementNum))
-## }
-
-
-
-
-
-
-
-Aupus(aupusFinalData = aupusFinal, shareData = shareData,
-      itemTypeCol = "measuredItemTypeFS", shareNum = "Value_share",
-      inputNum = "Value_input", balanceElementNum = "balanceElement")
-
-
-
-
-## Testing of the network standardization module
-load(".RData")
-load("share.RData")
-setnames(share, colnames(share), colnames(shareData))
-share[, `:=`(c("measuredItemParentFS", "measuredItemChildFS",
-               "geographicAreaFS"),
-             list(as.character(measuredItemParentFS),
-                  as.character(measuredItemChildFS),
-                  as.character(geographicAreaFS)))]
-shareDataOld = copy(share)
-setkeyv(shareDataOld, c("geographicAreaFS", "measuredItemParentFS",
-                        "measuredItemChildFS", "timePointYearsSP"))
-
-test.graph =
-    constructGraph(shareData = shareDataOld[timePointYearsSP == 2005, ],
-                   aupus = aupusData[timePointYearsSP == 2005, ],
-                   param = param, 
-                   shares = "Value_share",
-                   extractionRate = "Value_measuredElementFS_41",
-                   standardizeElement = "Value_measuredElementFS_61",
-                   plot = TRUE)
-
-
+## Construct the network for standardization
+standardizationGraph = 
+    with(updatedAupusNetwork,
+         constructStandardizationGraph(nodes = nodes, edges = edges,
+                                       standardizeElement = FBSelements,
+                                       from = param$keyNames$itemChildName,
+                                       to = param$keyNames$itemParentName))
 
-## The calculation looks fine, but write it on paper and also ask nick
-## to provide the control files.
-connected.graph = findConnectedGraph(test.graph, c("15"))
-connected.graph = connected.graph - V(connected.graph)["24"] - V(connected.graph)["17"] 
-plot(connected.graph, vertex.size = 8, edge.arrow.size = 0.5,
-     vertex.label = paste0(V(connected.graph)$name, "\n(",
-         V(connected.graph)$standardizeElement, ")"),
-     edge.label =
-         paste0(round(10000/E(connected.graph)$extractionRate, 2), "\n",
-                round(100/E(connected.graph)$shares, 2)))
-standardizeStep1 = standardizeNode(connected.graph,
-    names(which(degree(connected.graph, mode = "in") == 0 &
-                    degree(connected.graph, mode = "out") > 0)))
-with(standardizeStep1, 
-plot(standardizedGraph, vertex.size = 8, edge.arrow.size = 0.5,
-     vertex.label = paste0(V(standardizedGraph)$name, "\n(",
-         V(standardizedGraph)$standardizeElement, ")"),
-     edge.label = round(1000000/(E(standardizedGraph)$extractionRate * 
-                         E(standardizedGraph)$shares), 2)))
-standardizeStep2 = standardizeNode(standardizeStep1$standardizedGraph,
-    names(which(degree(standardizeStep1$standardizedGraph, mode = "in") == 0 &
-                    degree(standardizeStep1$standardizedGraph, mode = "out") > 0)))
-V(standardizeStep2$standardizedGraph)$standardizeElement
-
-
-
-element131.dt = aupusData[, c(key(aupusData), 
-    "Value_measuredElementFS_131"), with = FALSE]
-setnames(element131.dt, "measuredItemFS", "measuredItemParentFS")
-setkeyv(element131.dt, c("geographicAreaFS", "measuredItemParentFS",
-                         "timePointYearsSP"))
-
-inputWith131.dt = merge(inputData, element131.dt,
-    by = key(element131.dt), all.x = TRUE)
-setkeyv(inputWith131.dt, key(inputData))
-
-load("share.RData")
-setnames(share, colnames(share), colnames(shareData))
-share[, `:=`(c("measuredItemParentFS", "measuredItemChildFS",
-               "geographicAreaFS"),
-             list(as.character(measuredItemParentFS),
-                  as.character(measuredItemChildFS),
-                  as.character(geographicAreaFS)))]
-shareDataOld = copy(share)
-setkeyv(shareDataOld, c("geographicAreaFS", "measuredItemParentFS",
-                        "measuredItemChildFS", "timePointYearsSP"))
+## Standardize the data
+fbs =
+    fbsStandardization(graph = standardizationGraph,
+                       standardizeElement = FBSelements,
+                       plot = FALSE)
 
-inputFinal = merge(inputWith131.dt, shareDataOld,
-    by = key(inputWith131.dt), all = TRUE)
-inputCheck[, list(Check_input = Value_share/100 *
-                      Value_measuredElementFS_131)]
-
-## Take year 2005
-e = inputFinal[timePointYearsSP == 2005,
-    list(measuredItemParentFS, measureItemChildFS, Value_share)]
-
-## Element 131 belongs to parent and value_input belongs to child
-##
-## NOTE (Michael): There would be a problem if the child has value
-##                 much greater than parent.
-vp = inputFinal[timePointYearsSP == 2005,
-    list(measuredItemParentFS, Value_measuredElementFS_131)]
-vc = inputFinal[timePointYearsSP == 2005,
-    list(measuredItemChildFS, Value_input)]
-
-
-
-
-
-## Check the flags
-sapply(grep("flagFaostat", colnames(aupusData), value = TRUE),
-       FUN = function(x) table(aupusData[, x, with = FALSE]))
-
-
-
-## This function should be done by year.
-checkShareUnity = function(shareData, param){
-    checkShare =
-        shareData[, list(sum_check = sum(Value_share)),
-                  by = c(param$keyNames$countryName,
-                      param$keyNames$itemChildName,
-                      param$keyNames$yearName)]
-    checkShare[sum_check != 100, ]
-}
-
-checkShareUnity(shareDataOld, param)
-
-
-test = by(aupusData, aupusData$timePointYearsSP, buildEdges, )
-
-
-
-aupus2005 = aupusData[timePointYearsSP == 2005, ]
-share2005 = shareDataOld[timePointYearsSP == 2005, ]
-input2005 = inputData[timePointYearsSP == 2005, ]
-aupusEdges =
-    buildEdges(aupusData = aupus2005,
-               extractionRate = "Value_measuredElementFS_41",
-               shareData = share2005,
-               inputData = input2005,
-               param = param)
-
-aupusNodes =
-    buildNodes(aupusData = aupus2005, ratioData = ratioData,
-               balanceElementData = balanceElementData,
-               itemInfoData = itemInfoData,
-               balanceElementNum = "balanceElement")
-
-
-processDownLevel =
-    findProcessingLevel(aupusEdges,
-                        from = "measuredItemParentFS",
-                        to = "measuredItemChildFS",
-                        plot = TRUE)
-
-matchProcessingLevel = function(processingLevel, item){
-    matchedLevel = processingLevel[match(item, names(processingLevel))]
-    ## If it is not in the edge, then it is an item which has no
-    ## relationship with others and can be processed immediately.
-    matchedLevel[is.na(matchedLevel)] = 0
-    matchedLevel
-}
-
-
-aupusNodes[, processingLevel :=
-               matchProcessingLevel(processDownLevel, measuredItemFS)]
-
-
-
-## These 3 elements need to be propagated at every step.
-##
-## The share data in the Aupus is only for element 66 and 96, thus we
-## can take it out with element66 and 96 as standalone module.
-##
-## The input is only used by element 31 and updating input from processing.
-##
-
-
 
-## This is a temporary solution for converting the population data
-## assuming we have used getPopulation
-library(faoswsAupus)
-library(faoswsUtil)
-library(data.table)
-library(igraph)
 
-load(".RData")
-populationData =
-    aupusData[measuredItemFS == '1', c(key(aupusData),
-                  "Value_measuredElementFS_11", "Value_measuredElementFS_21"),
-              with = FALSE]
-setkeyv(populationData, c("geographicAreaFS", "timePointYearsSP"))
-## NOTE (Michael): Looks like the key eliminates the duplicates
-populationData[, measuredItemFS := NULL]
-aupusData = aupusData[measuredItemFS != '1', ]
-
-
-okey = key(aupusData)
-setkeyv(aupusData, key(populationData))
-aupusData[populationData, `:=`(c("Value_population_11", "Value_population_21"),
-                               list(i.Value_measuredElementFS_11,
-                                    i.Value_measuredElementFS_21))]
-setkeyv(aupusData, okey)
-
-## This is the temporary solution before we update the share data
-load("share.RData")
-setnames(share, colnames(share), colnames(shareData))
-share[, `:=`(c("measuredItemParentFS", "measuredItemChildFS",
-               "geographicAreaFS"),
-             list(as.character(measuredItemParentFS),
-                  as.character(measuredItemChildFS),
-                  as.character(geographicAreaFS)))]
-shareDataOld = copy(share)
-setkeyv(shareDataOld, c("geographicAreaFS", "measuredItemParentFS",
-                        "measuredItemChildFS", "timePointYearsSP"))
-
-
-
-extractionRateData =
-    aupusData[, c(key(aupusData), "Value_measuredElementFS_41"), with = FALSE]
-setnames(x = extractionRateData,
-         old = c("measuredItemFS", "Value_measuredElementFS_41"),
-         new = c("measuredItemChildFS", "Value_extraction"))
-setkeyv(extractionRateData,
-        cols = c("geographicAreaFS", "measuredItemChildFS", "timePointYearsSP"))
-
-
-## These are temporary hacks for names before updated the data
-param$keyNames = list(areaName = "geographicAreaFS",
-             itemName = "measuredItemFS",
-             itemParentName = "measuredItemParentFS",
-             itemChildName = "measuredItemChildFS",
-             itemTypeName = "measuredItemTypeFS",
-             elementName = "measuredElementFS",
-             extractionRateName = "Value_extractionRate",
-             balanceElementName = "Value_balanceElement",
-             inputName = "Value_input",
-             shareName = "Value_share",
-             yearName = "timePointYearsSP",
-             valuePrefix = "Value_",
-             flagPrefix = "flagFaostat_",
-             ratioPrefix = "Ratio_")
-
-lapply(balanceElementData, function(x)
-    setnames(x, old = "balanceElement",
-             new = param$keyNames$balanceElementName))
-
-
-
-
- 
-foo = function(nodes, edges, ...){
-
-
-    ## Step (1): Run the aupus module at the primary level on the nodes
-    Aupus(aupusFinalData = nodes,
-          itemTypeCol = param$keyNames$itemTypeName,
-          balanceElementNum = param$keyNames$balanceElementName)
 
-    ## Step (2): Update the edges (extraction rate and input from processing)
-    updateEdges(nodes = nodes, 
-                edges = edges,
-                element41Num = with(param$keyNames,
-                    paste0(valuePrefix, elementName, "_41")),
-                element131Num = with(param$keyNames,
-                    paste0(valuePrefix, elementName, "_131")))
 
-    ## Step (3): Propagate input from processing to the node
-    updateInputFromProcessing(nodes = nodes,
-                              edges = edges,
-                              element31Num = "Value_measuredElementFS_31")
-}
 
 
 
+## test.nodes = data.table(name = letters[1:5], values = 1:5, value2 = rnorm(5))
+## test.edges = data.table(from = letters[c(1, 3, 5)], to = letters[1], value = 1:3)
 
+## test.graph = graph.data.frame(d = test.edges, vertices = test.nodes)
 
+## matrix(unlist(lapply(list.vertex.attributes(test.graph), function(x) get.vertex.attribute(test.graph, x))[2:3]), nc = 2)
 
-    
-
-constructStandardizationGraph = function(nodes, edges,
-    standardizeElement = c("Value_measuredElementFS_61",
-        "Value_measuredElementFS_91"), from, to, ...){
-
-    nodeCopy = copy(nodes)
-    edgeCopy = copy(edges)
-    edgeCopy = edgeCopy[, c(from, to, colnames(edgeCopy)[!colnames(edgeCopy) %in%
-        c(from, to)]), with = FALSE]
-    setnames(edgeCopy, to, param$keyNames$itemName)
-    
-    uniqueYears = unique(unique(nodeCopy$timePointYearsSP),
-        unique(edgeCopy$timePointYearsSP))
-    edgeCopy[, `:=`("geographicAreaFS", NULL)]
-    edgeCopy[, `:=`("timePointYearsSP", NULL)]
-    nodes.lst = split(nodeCopy, uniqueYears)
-    edges.lst = split(edgeCopy, uniqueYears)
-
-    nodes.lst =
-        lapply(nodes.lst,
-               FUN = function(x){
-                   x[, c("measuredItemFS", standardizeElement), with = FALSE]
-               })
-    
-    graph.lst = mapply(graph.data.frame, d = edges.lst, vertices = nodes.lst,
-        SIMPLIFY = FALSE)
-    graph.lst
-}
-
-
-system.time({
-test =
-    suaToNetworkStructure(extractionRateData = extractionRateData,
-                          shareData = shareDataOld,
-                          inputData = inputData,
-                          ratioData = ratioData,
-                          balanceElementData = balanceElementData,
-                          itemInfoData = itemInfoData,
-                          from = param$keyNames$itemParentName,
-                          to = param$keyNames$itemChildName)
-
-
-
-for(i in range(test$nodes$processingLevel)){
-    with(test, foo(nodes = nodes[processingLevel == i, ], edges = edges))
-}
-
-
-FBSelements = c("Value_measuredElementFS_61", "Value_measuredElementFS_91",
-    "Value_measuredElementFS_111",
-    "Value_measuredElementFS_121", "Value_measuredElementFS_141")
-
-allGraph = with(test,
-    constructStandardizationGraph(nodes = nodes, edges = edges,
-                                  standardizeElement = FBSelements,
-                                  from = param$keyNames$itemChildName,
-                                  to = param$keyNames$itemParentName))
-
-## The edge should be reversed for standardization.
-standardizationTest =
-    lapply(allGraph, FUN = function(x){
-        standardization(graph = x,
-                        standardizeElement = FBSelements,
-                        plot = FALSE)
-    })
-
-
-lapply(names(standardizationTest),
-       FUN = function(x){
-           standardizationTest[[x]][, `:=`(c("timePointYearsSP"), as.numeric(x))]
-       })
-
-
-standardizationFinal = Reduce(rbind, standardizationTest)
-
-})
-
-
-
-
-
-
-
-test.nodes = data.table(name = letters[1:5], values = 1:5, value2 = rnorm(5))
-test.edges = data.table(from = letters[c(1, 3, 5)], to = letters[1], value = 1:3)
-
-test.graph = graph.data.frame(d = test.edges, vertices = test.nodes)
-
-matrix(unlist(lapply(list.vertex.attributes(test.graph), function(x) get.vertex.attribute(test.graph, x))[2:3]), nc = 2)
-
-Reduce(function(x, y) cbind(data.frame(x), data.frame(y)), lapply(list.vertex.attributes(test.graph), FUN = function(x) get.vertex.attribute(test.graph, x)))
+## Reduce(function(x, y) cbind(data.frame(x), data.frame(y)), lapply(list.vertex.attributes(test.graph), FUN = function(x) get.vertex.attribute(test.graph, x)))
 
 
 
