72c2349c0b92ae2fabb20f10689d411b2c3973bd
mkao006 <mkao006@gmail.com>
2014-11-26 18:23:30 +0100

    added population data in the argument

--- codes/buildNodes.R
+++ codes/buildNodes.R
@@ -14,7 +14,7 @@
 ##' 
 
 buildNodes = function(aupusData, ratioData, balanceElementData, itemInfoData,
-    balanceElementNum, verbose = FALSE){
+    balanceElementNum, populationData, verbose = FALSE){
     aupusWithInfo = mergeItemInfo(aupusData, itemInfoData)
     aupusWithInfoRatio = mergeRatio(aupusData = aupusWithInfo, 
         ratioData = ratioData, verbose = varbose)
@@ -23,6 +23,13 @@ buildNodes = function(aupusData, ratioData, balanceElementData, itemInfoData,
         balanceElementData = balanceElementData,
         balanceElementNum = balanceElementNum, 
         verbose = verbose)
+
+    okey = key(aupusFinal)
+    setkeyv(aupusFinal, key(populationData))
+    aupusFinal[populationData,
+                     `:=`(c("Value_population_11", "Value_population_21"),
+                          list(i.Value_population_11,
+                               i.Value_population_21))]
     setkeyv(aupusFinal, key(aupusData))
     aupusFinal  
 }
