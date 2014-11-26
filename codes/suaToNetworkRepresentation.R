d76d2589d98dc9b58393a1a6b3416353368cfb14
mkao006 <mkao006@gmail.com>
2014-11-26 18:28:38 +0100

    First commit of the function to convert original data to network representation

---
+++ codes/suaToNetworkRepresentation.R
@@ -0,0 +1,57 @@
+##' This is the function to convert sua data to network specification
+##' which is more natural for processing.
+##'
+##'
+##' @param extractionRateData The extraction rate data
+##' @param shareData The share data from getShare function
+##' @param inputData The input data from the function getInputFromProcessing.
+##' @param ratioData The ratio data from getRatio
+##' @param balanceElementData The balance element data from the
+##' function getBalanceElementData.
+##' @param itemInfoData
+##' @export
+##' 
+
+
+
+suaToNetworkRepresentation = function(extractionRateData, shareData, inputData,
+    ratioData, balanceElementData, itemInfoData, populationData){
+
+    edges =
+        buildEdges(extractionRateData = extractionRateData,
+                   shareData = shareData,
+                   inputData = inputData)
+
+    nodes =
+        buildNodes(aupusData = aupusData, ratioData = ratioData,
+                   balanceElementData = balanceElementData,
+                   itemInfoData = itemInfoData,
+                   populationData = populationData,
+                   balanceElementNum = param$keyNames$balanceElementName)
+
+    ## CHECK (Michael): Add in nodes that are contained in the edges
+    ##                  list but not found in the node frame. Check
+    ##                  why they are missing.
+
+    uniqueEdgeNodes =
+        unique(unlist(edges[, c(param$keyNames$itemParentName,
+                                param$keyNames$itemChildName), with = FALSE]))
+
+    missingNodes = uniqueEdgeNodes[!uniqueEdgeNodes %in%
+        nodes[[param$keyNames$itemName]]]
+
+    missingKeyTable =
+        data.table(expand.grid(unlist(unique(nodes[, c(param$keyNames$areaName),
+                                                   with = FALSE])),
+                               unlist(unique(nodes[, c(param$keyNames$yearName),
+                                                   with = FALSE])),
+                               missingNodes, stringsAsFactors = FALSE))
+    with(param$keyNames,
+         setnames(missingKeyTable, c(areaName, yearName, itemName)))
+    nodes = rbind(nodes, missingKeyTable, fill = TRUE)
+    setkeyv(nodes, key(aupusData))
+    
+    
+
+    list(nodes = nodes, edges = edges)
+}
